59beed550fc7362f5ad0ba074f22f597cae90b62
Michael Lawrence <m6lawrence1@gmail.com>
2016-04-03 16:22:41 -0400

    Updated to make total price fixed to 2 decimal places and added $ symbol

--- style.js
+++ style.js
@@ -1,26 +1,26 @@
 var myShoppingList = [
 
    {
-   	name: "apple",
-	price: 1,
-	},
+    name: "apple",
+  price: 1,
+  },
 
     {
-   	name: "tofu",
-	price: 3.55,
-	},
+    name: "tofu",
+  price: 3.55,
+  },
 
-	{
-   	name: "wine",
-	price: 9.99,
-	},
-	{
-   	name: "beer",
-	price: 11.75,
-	},
+  {
+    name: "wine",
+  price: 9.99,
+  },
+  {
+    name: "beer",
+  price: 11.75,
+  },
 ]
 
-var total = 0; //creating global variable total to save value of total from forEach function, 
+var total = 0.00; //creating global variable total to save value of total from forEach function, 
               //in order to reference it in second function to get grand total 
 
 
@@ -39,7 +39,7 @@ myShoppingList.forEach(function(element){ // element represents each object in a
 
   var listTotal= document.getElementById("salmon"); //sets variable 'listTotal' to id 'salmon' 
 
-  listTotal.innerHTML = total; // adds value of total to html of id 'salmon'
+  listTotal.innerHTML = "$" + total; // adds value of total to html of id 'salmon'
 
 });
 
@@ -57,14 +57,14 @@ function myFunction() {
   var foodSomething= document.getElementById("food").appendChild(foodItem); // sets variable 'foodSomething' = to id 'food' and appends 'foodItem' to id 'food'
   
   food.insertBefore(foodItem, food.firstChild); //appends 'foodItem' before firstChild of id 'food'
-  
 
+//Part of function for calculating total price for forEach function total and new items added
   var foodPriceNum = Number(inputPrice); // converts string value of 'inputPrice' to a number in order to add prices together
 
-  var listTotal= document.getElementById("salmon"); // sets variable 'listTotal' to value of id ''
+  var listTotal = total += foodPriceNum; // variable to calculate new total after each item is added
 
-  listTotal.innerHTML = total += foodPriceNum; //adds 'total' from forEach function to 'foodPriceNum' total from 'inputPrice' value
-}
+  var pleaseWork = document.getElementById('salmon'); // sets variable 'pleaseWork' = to id 'salmon'
 
+  pleaseWork.innerHTML = "$" + listTotal.toFixed(2); // places html variable 'pleaseWork' in id 'salmon'
 
-
+};
